03c073265eac9bd99fe2cef02c5b2c2ed855807b
yihang99 <61446830+yihang99@users.noreply.github.com>
2021-05-18 22:40:39 +0800

    Add get_value_list()

--- perlin.py
+++ perlin.py
@@ -4,7 +4,7 @@ import cv2
 import pyredner
 
 
-class Perlin2D:
+class Perlin2D(object):
     def __init__(self, interval=1.,
                  range_x: int = 1,
                  range_y: int = 1,
@@ -46,21 +46,61 @@ class Perlin2D:
                 + v3 * f(xr - x) * f(y - yl) + v4 * f(xr - x) * f(yr - y)
         return value
 
+    def get_value_list(self, point_list: torch.Tensor):
+        pos = point_list / self.interval
+
+        pos_ll = pos.floor().to(torch.int64)
+        pos_rr = pos_ll + 1
+        pos_lr = pos_ll + torch.tensor([0, 1])
+        pos_rl = pos_ll + torch.tensor([1, 0])
+        tx_l = pos[:, 0] - pos_ll[:, 0]
+        tx_r = pos[:, 0] - pos_rr[:, 0]
+        ty_l = pos[:, 1] - pos_ll[:, 1]
+        ty_r = pos[:, 1] - pos_rr[:, 1]
+
+        v1 = self.value[pos_ll[:, 0], pos_ll[:, 1]] + self.grad_x[pos_ll[:, 0], pos_ll[:, 1]] * tx_l + self.grad_y[
+            pos_ll[:, 0], pos_ll[:, 1]] * ty_l
+        v2 = self.value[pos_lr[:, 0], pos_lr[:, 1]] + self.grad_x[pos_lr[:, 0], pos_lr[:, 1]] * tx_l + self.grad_y[
+            pos_lr[:, 0], pos_lr[:, 1]] * ty_r
+        v3 = self.value[pos_rl[:, 0], pos_rl[:, 1]] + self.grad_x[pos_rl[:, 0], pos_rl[:, 1]] * tx_r + self.grad_y[
+            pos_rl[:, 0], pos_rl[:, 1]] * ty_l
+        v4 = self.value[pos_rr[:, 0], pos_rr[:, 1]] + self.grad_x[pos_rr[:, 0], pos_rr[:, 1]] * tx_r + self.grad_y[
+            pos_rr[:, 0], pos_rr[:, 1]] * ty_r
+
+        def f(t):
+            return 1 - 3 * t ** 2 + 2 * t ** 3
+
+        value = v1 * f(tx_l) * f(ty_l) + v2 * f(tx_l) * f(-ty_r) \
+                + v3 * f(-tx_r) * f(ty_l) + v4 * f(-tx_r) * f(-ty_r)
+        return value
+
     def get_map(self, range_x, range_y, interval):
         ret = torch.zeros(range_x, range_y)
-        y = torch.tensor(range(range_y)).expand(range_x, -1) * interval / self.interval
-        x = torch.tensor(range(range_x)).reshape(-1, 1).expand(-1, range_y) * interval / self.interval
+        x = torch.tensor(range(range_x)) * interval / self.interval
+        y = torch.tensor(range(range_y)) * interval / self.interval
+
         xl = x.floor().to(torch.int64)
         yl = y.floor().to(torch.int64)
         xr = xl + 1
         yr = yl + 1
-        #  not finished yet
 
-p = Perlin2D(interval=20., range_x=100, range_y=100, sigma_of_grad=0.2, sigma_of_value=0.1)
-# img = np.zeros((100, 100))
-# for i in range(100):
-#     for j in range(100):
-#         img[i, j] = p.get_value(i, j) + 0.5
-# cv2.imshow("img", img)
-# cv2.imwrite("perlin.jpg", 255 * img)
-# cv2.waitKey(200)
+        x = x.reshape(-1, 1).expand(-1, range_y)
+        y = y.expand(range_x, -1)
+        xll = xl.reshape(-1, 1).expand(-1, range_y)
+        yll = yl.expand(range_x, -1)
+        xrr = xr.reshape(-1, 1).expand(-1, range_y)
+        yrr = yr.expand(range_x, -1)
+
+        v1 = self.value[xl][:, yl] + self.grad_x[xl][:, yl] * (x - xll) + self.grad_y[xl][:, yl] * (y - yll)
+        v2 = self.value[xl][:, yr] + self.grad_x[xl][:, yr] * (x - xll) + self.grad_y[xl][:, yr] * (y - yrr)
+        v3 = self.value[xr][:, yl] + self.grad_x[xr][:, yl] * (x - xrr) + self.grad_y[xr][:, yl] * (y - yll)
+        v4 = self.value[xr][:, yr] + self.grad_x[xr][:, yr] * (x - xrr) + self.grad_y[xr][:, yr] * (y - yrr)
+
+        def f(t):
+            return 1 - 3 * t ** 2 + 2 * t ** 3
+
+        value = v1 * f(x - xll) * f(y - yll) + v2 * f(x - xll) * f(yrr - y) \
+                + v3 * f(xrr - x) * f(y - yll) + v4 * f(xrr - x) * f(yrr - y)
+        return value
+
+
